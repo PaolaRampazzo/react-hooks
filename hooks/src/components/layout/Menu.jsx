aa2430cf32dc5edd837768358510300a30f257c2
PaolaRampazzo <paolatrampazzo@gmail.com>
2021-09-12 00:48:28 +0000

    Aula 05 - Hooks

--- hooks/src/components/layout/Menu.jsx
+++ hooks/src/components/layout/Menu.jsx
@@ -7,20 +7,17 @@ const Menu = props => (
         <nav>
             <ul>
                 <li> 
-                    <Link to="/">Home</Link>
+                    <Link to="/">Hooks</Link>
                 </li>
                 <li> 
-                    <Link to="/conteudo01/123">Conteúdo 01</Link>
+                    <Link to="/useState">useState()</Link>
                 </li>
                 <li> 
-                    <Link to="/conteudo02">Conteúdo 02</Link>
+                    <Link to="/useEffect">useEffect()</Link>
                 </li>                                
                 <li> 
-                    <Link to="/conteudo03">Conteúdo 03</Link>
-                </li>
-                <li> 
-                    <Link to="/conteudo04">Conteúdo 04</Link>
-                </li>                                
+                    <Link to="/useContext">useContext()</Link>
+                </li>                             
             </ul>
         </nav>
     </aside>
